3b97d6caa4ef128d62f0b295e741c000ded5456a
Shubham kumar choudhary <shubhamchoudhary7114@gmail.com>
2021-10-19 19:31:48 +0530

    Spring AOP Pointcut Declaration code added

---
+++ src/springaop/demo/DemoConfig.java
@@ -0,0 +1,12 @@
+package springaop.demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("springaop")
+public class DemoConfig {
+
+}
---
+++ src/springaop/demo/MainDemoApp.java
@@ -0,0 +1,54 @@
+package springaop.demo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springaop.dao.AccountDAO;
+import springaop.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+
+
+		// read spring config java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+		// get membership bean from spring container
+		MembershipDAO theMembershipDAO =
+				context.getBean("membershipDAO", MembershipDAO.class);
+
+		// call the business method
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+
+		//Calling getter and setter method
+
+		theAccountDAO.setName("foobar");
+		theAccountDAO.setServiceCode("Silver");
+
+		String name=theAccountDAO.getName();
+		String code=theAccountDAO.getServiceCode();
+
+		// call the membership business method
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+
+		// close the context
+		context.close();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
